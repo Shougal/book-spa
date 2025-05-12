c2500fcd7b6b9ca7ca67c4802a0695805d135ca2
shougal <xdq9qa@virginia.edu>
2025-05-12 13:50:56 -0400

    reformatted and cleaned code

--- src/app/api/GutAPI/route.ts
+++ src/app/api/GutAPI/route.ts
@@ -9,18 +9,19 @@ export async function GET(request: Request){
     for(let i=0; i<content_url.length; i++){
         const response = await fetch(content_url[i]);
         if(response.ok && response.status!== 404){
-            console.log(`got conetnt for:  ${content_url}`);
+            
             const data = await response.text();
+            
             return new Response(data,{
                 status: 200,
                 headers:{
                     "Content-Type": "text/plain",
                 },
             });
-            // return data;
+            
         }
     }
-    console.log("did not return");
+    
 
     return new Response(null, {status: 404});
 }
