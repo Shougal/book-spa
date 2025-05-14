cfcce6eae2e1c11b355cf8f54599ed488850f334
shougal <xdq9qa@virginia.edu>
2025-05-14 12:00:45 -0400

    add Mermaid class component with SVG rendering and error handling

--- src/components/Mermaid.tsx
+++ src/components/Mermaid.tsx
@@ -6,15 +6,38 @@ import mermaid from 'mermaid';
 // Defining props type
 type MermaidProps = {
   chart: string;
+  onError?: () => void;
 };
 
 // Use it with the component
 export default class Mermaid extends React.Component<MermaidProps> {
-  componentDidMount() {
-    mermaid.contentLoaded();
-  }
-
-  render() {
-    return <div className="mermaid">{this.props.chart}</div>;
-  }
+    containerRef = React.createRef<HTMLDivElement>();
+    componentDidMount() {
+        this.renderMermaid();
+    }
+    componentDidUpdate(prevProps: MermaidProps) {
+        if (prevProps.chart !== this.props.chart) {
+          this.renderMermaid();
+        }
+      }
+    
+    renderMermaid() {
+        mermaid.initialize({ startOnLoad: false });
+        const { chart, onError } = this.props;
+        mermaid
+          .render('generatedChart', chart)
+          .then(({ svg }) => {
+            if (this.containerRef.current) {
+              this.containerRef.current.innerHTML = svg;
+            }
+          })
+          .catch((error) => {
+            console.error('Mermaid rendering error:', error);
+            if (onError) onError();
+          });
+      }
+      
+    render() {
+        return <div ref={this.containerRef}></div>;
+    }
 }
